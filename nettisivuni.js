22ba7f3e2a3fa1bd34a067f8d5e7b7bfa5199a12
atte-pp <piiroinen_97@hotmail.com>
2025-12-06 13:45:22 +0200

    muokkasin vähän

--- nettisivuni.js
+++ nettisivuni.js
@@ -3,6 +3,7 @@ function showPic() {
 
 const img = document.createElement("img");
 img.src = "/kuvia/gorillakeskisormi.jpg";
+img.width = 650;
 container.innerHTML = "";
 container.appendChild(img);
 }
